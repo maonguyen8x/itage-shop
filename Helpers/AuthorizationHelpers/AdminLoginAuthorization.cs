b64407d4fb56e5ac00f0ab2b1fdbde4fd26a3573
maonguyen8x <nguyenmao.2912@gmail.com>
2024-11-12 00:00:15 +0900

    Initial source code

---
+++ Helpers/AuthorizationHelpers/AdminLoginAuthorization.cs
@@ -0,0 +1,48 @@
+﻿
+using Entities.DBInheritedModels;
+using Entities.DBModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.AuthorizationHelpers
+{
+    public class AdminLoginAuthorization : ActionFilterAttribute
+    {
+        private readonly ISessionManager _sessionManag;
+        public AdminLoginAuthorization(ISessionManager sessionManag)
+        {
+            _sessionManag = sessionManag;
+        }
+
+       
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            UserEntity? usr = new UserEntity();
+
+            usr = _sessionManag.GetLoginUserFromSession();
+
+
+            // --if user session null, then redirect to  login page
+            if (usr == null || usr.UserId < 1)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    controller = "Authentication",
+                    action = "Login",
+                    area = ""
+                }));
+
+            }
+
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# itage-shop
+## React.Web
+```bash
+$	yarn install || npm install
+$	yarn dev || npm start
+```
+
+### AdminPanel
+1. Config appsettings.json
+2. 
